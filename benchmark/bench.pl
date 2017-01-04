34c4fb174b35326310734847887cc2c9302f98be
Shawn Laffan <shawnlaffan@gmail.com>
2017-01-05 08:56:33 +1100

    More benchmark tweaking

--- benchmark/bench.pl
+++ benchmark/bench.pl
@@ -19,35 +19,46 @@ use Statistics::Sampler::Multinomial;
 use Math::Random qw/random_multinomial/;
 use Math::GSL::Randist qw /gsl_ran_multinomial/;
 use Math::GSL::RNG qw /gsl_rng_uniform $gsl_rng_mt19937/;
+use Math::Random::MT::Auto;
+use Math::Random::MTwist;
 
-my $max = 1000;
-my @data = map {int (rand() * $max)} (1 .. $max);
+srand(2345);
+my $max = 100;
+my $nsamples = 1000;
+my @data = map {int (rand() * $max)} (1 .. $nsamples);
 
-foreach my $K (10, 100, 1000) {
-#foreach my $K (10, 100) {
+#foreach my $K (10, 100, 1000) {
+#foreach my $K (10, 50, 100) {
+foreach my $K (10) {
     my @subset = @data[0..($K-1)];
     my $sum = sum @subset;
     my $scaled_data = [map {$_ / $sum} @subset];
 
+    say "Data are: " . join ' ', @subset;
+
     my $gsl_rng = Math::GSL::RNG->new($gsl_rng_mt19937);
 
     #  initialised version using default PRNG
-    my $SSMi = Statistics::Sampler::Multinomial->new ();
+    my $SSMi = Statistics::Sampler::Multinomial->new (
+        prng => Math::Random::MT::Auto->new,
+        #prng => Math::Random::MTwist->new,  #  about 2x faster than MRMA
+    );
     $SSMi->initialise (data => $scaled_data, data_sum_to_one => 1);
 
     #  uninitialised
     my $SSMu = Statistics::Sampler::Multinomial->new;
 
-    my $N = $K;
+    my $N = $K * 10;
+    $N = $sum;
 
-    say "$N from $sum";
+    say "Repeatedly drawing $N samples from $sum items across $K classes";
     
     #randist($gsl_rng, $N, $scaled_data);
     #SSMi_draw($SSMi, $N, $scaled_data);
     #math_random(undef, $N, $scaled_data);
 
     cmpthese (
-        -2,
+        -3,
         {
             #  all get the same number of args
             randist => sub {randist($gsl_rng, $N, $scaled_data)},
